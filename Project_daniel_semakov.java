adddc780c57116a3dac381fe7bd07d3bafc596ff
DanielSemakov <144864619+DanielSemakov@users.noreply.github.com>
2023-10-28 06:20:01 -0400

    Made class work with PolicyHolder class

--- Project_daniel_semakov.java
+++ Project_daniel_semakov.java
@@ -5,78 +5,82 @@ import java.io.FileNotFoundException;
 
 public class Project_daniel_semakov {
 
-   public static void main(String[] args) {
-      File textFile = new File("PolicyInformation.txt");
+    public static void main(String[] args) {
+        File textFile = new File("PolicyInformation.txt");
 
-      Scanner scanner;
-      try {
-         scanner = new Scanner(textFile);
-      } catch (FileNotFoundException e) {
-         e.printStackTrace();
-         scanner = new Scanner(System.in);
-      }
+        Scanner scanner;
+        try {
+            scanner = new Scanner(textFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            scanner = new Scanner(System.in);
+        }
 
-      ArrayList<Policy> policies = new ArrayList<>();
+        ArrayList<Policy> policies = new ArrayList<>();
 
-      while (scanner.hasNext()) {
-         int policyNum = Integer.parseInt(scanner.nextLine());
-          String providerName = scanner.nextLine();
+        while (scanner.hasNext()) {
+            int policyNum = Integer.parseInt(scanner.nextLine());
+            String providerName = scanner.nextLine();
 
-          String firstName = scanner.nextLine();
-          String lastName = scanner.nextLine();
-          int age = Integer.parseInt(scanner.nextLine());
+            String firstName = scanner.nextLine();
+            String lastName = scanner.nextLine();
+            int age = Integer.parseInt(scanner.nextLine());
 
-          String smokingStatus = scanner.nextLine();
+            String smokingStatus = scanner.nextLine();
 
-          double heightInches = scanner.nextDouble();
-          double weightLbs = scanner.nextDouble();
+            double heightInches = scanner.nextDouble();
+            double weightLbs = scanner.nextDouble();
 
-          Policy policy = new Policy(policyNum, providerName, firstName, lastName, age, smokingStatus, heightInches,
-          weightLbs);
+            PolicyHolder policyHolder = new PolicyHolder(firstName, lastName, age, smokingStatus, heightInches,
+                    weightLbs);
+            Policy policy = new Policy(policyNum, providerName, policyHolder);
 
-          policies.add(policy);
+            policies.add(policy);
 
-          if (scanner.hasNext()) {
-            scanner.nextLine();
-            scanner.nextLine();
-          }
-      }
-      
-      for (Policy policy: policies) {
-         System.out.printf("Policy Number: %d%n", policy.getPolicyNum());
-          System.out.printf("Provider Name: %s%n", policy.getProviderName());
+            if (scanner.hasNext()) {
+                scanner.nextLine();
+                scanner.nextLine();
+            }
+        }
 
-          System.out.printf("Policyholder's First Name: %s%n", policy.getPolicyHolderFirstName());
-          System.out.printf("Policyholder's Last Name: %s%n", policy.getPolicyHolderLastName());
-          System.out.printf("Policyholder's Age: %d%n", policy.getPolicyHolderAge());
+        for (Policy policy : policies) {
+            System.out.printf("Policy Number: %d%n", policy.getPolicyNum());
+            System.out.printf("Provider Name: %s%n", policy.getProviderName());
 
-          System.out.printf("Policyholder's Smoking Status: %s%n", policy.getSmokingStatus());
+            PolicyHolder policyHolder = policy.getPolicyHolder();
+            
+            System.out.printf("Policyholder's First Name: %s%n", policyHolder.getFirstName());
+            System.out.printf("Policyholder's Last Name: %s%n", policyHolder.getLastName());
+            System.out.printf("Policyholder's Age: %d%n", policyHolder.getAge());
 
-          System.out.printf("Policyholder's Height: %.1f inches%n", policy.getPolicyHolderHeightInches());
-          System.out.printf("Policyholder's Weight: %.1f pounds%n", policy.getPolicyHolderWeightPounds());
-          System.out.printf("Policyholder's BMI: %.2f%n", policy.getPolicyHolderBMI());
+            System.out.printf("Policyholder's Smoking Status: %s%n", policyHolder.getSmokingStatus());
 
-          System.out.printf("Policy Price: $%.2f%n", policy.getPrice());
+            System.out.printf("Policyholder's Height: %.1f inches%n", policyHolder.getHeightInches());
+            System.out.printf("Policyholder's Weight: %.1f pounds%n", policyHolder.getWeightPounds());
+            System.out.printf("Policyholder's BMI: %.2f%n", policyHolder.getBMI());
 
-          System.out.println();
-      }
-      
-      displaySmokingStatuses(policies);
-   }
-   
-   public static void displaySmokingStatuses(ArrayList<Policy> policies) {
-      int numSmokers = 0;
-      int numNonSmokers = 0;
-      
-      for (Policy policy: policies) {
-         if (policy.getSmokingStatus().equals("smoker")) {
-            numSmokers++;
-         } else {
-            numNonSmokers++;
-         }
-      }
-      
-      System.out.println("The number of policies with a smoker is: " + numSmokers);
-      System.out.println("The number of policies with a non-smoker is: " + numNonSmokers);
-   }
+            System.out.printf("Policy Price: $%.2f%n", policy.getPrice());
+
+            System.out.println();
+        }
+
+        displaySmokingStatuses(policies);
+    }
+
+    public static void displaySmokingStatuses(ArrayList<Policy> policies) {
+        int numSmokers = 0;
+        int numNonSmokers = 0;
+
+        for (Policy policy : policies) {
+            PolicyHolder policyHolder = policy.getPolicyHolder();
+            if (policyHolder.getSmokingStatus().equals("smoker")) {
+                numSmokers++;
+            } else {
+                numNonSmokers++;
+            }
+        }
+
+        System.out.println("The number of policies with a smoker is: " + numSmokers);
+        System.out.println("The number of policies with a non-smoker is: " + numNonSmokers);
+    }
 }
